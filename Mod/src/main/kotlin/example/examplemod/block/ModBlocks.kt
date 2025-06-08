db3f7600b976168190cb84c7f03f8113d8bf0e29
Avangardum <yurykuz65@gmail.com>
2025-06-08 19:08:09 +0300

    Remove example code

--- Mod/src/main/kotlin/example/examplemod/block/ModBlocks.kt
+++ Mod/src/main/kotlin/example/examplemod/block/ModBlocks.kt
@@ -1,8 +1,6 @@
 package example.examplemod.block
 
 import example.examplemod.EuclideanLogisticsMod
-import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.block.state.BlockBehaviour
 import net.neoforged.neoforge.registries.DeferredRegister
 
 // THIS LINE IS REQUIRED FOR USING PROPERTY DELEGATES
@@ -10,9 +8,4 @@ import thedarkcolour.kotlinforforge.neoforge.forge.getValue
 
 object ModBlocks {
     val REGISTRY = DeferredRegister.createBlocks(EuclideanLogisticsMod.ID)
-
-    // If you get an "overload resolution ambiguity" error, include the arrow at the start of the closure.
-    val EXAMPLE_BLOCK by REGISTRY.register("example_block") { ->
-        Block(BlockBehaviour.Properties.of().lightLevel { 15 }.strength(3.0f))
-    }
 }
