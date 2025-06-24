d48f16dd43bb451b0062593a7e863798bb345744
Avangardum <yurykuz65@gmail.com>
2025-06-24 19:19:10 +0300

    Disable inventory slots other than 0

---
+++ Mod/src/main/java/net/avangardum/euclideanlogistics/mixins/MixinInventory.java
@@ -0,0 +1,33 @@
+package net.avangardum.euclideanlogistics.mixins;
+
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(Inventory.class)
+public abstract class MixinInventory implements Container {
+    @Shadow @Final public NonNullList<ItemStack> items;
+    @Shadow public int selected;
+
+    @Overwrite
+    public int getFreeSlot() {
+        return this.items.getFirst().isEmpty() ? 0 : -1;
+    }
+
+    @Override
+    public int getMaxStackSize(@NotNull ItemStack stack) {
+        return 1;
+    }
+
+    /** Handle mouse scroll. */
+    @Overwrite
+    public void swapPaint(double direction) {
+        selected = 0;
+    }
+}
